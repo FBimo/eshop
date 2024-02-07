ac3fc13db9ec8d7a7273861f67a7cfd49e4ca8d6
FBimo <f.bimo31@gmail.com>
2024-02-07 17:28:53 +0700

    fix productQuantity data type

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -3,8 +3,9 @@ package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Getter;
 import lombok.Setter;
 
+@Getter @Setter
 public class Product {
     private String productId;
     private String productName;
-    private String productQuantity;
+    private int productQuantity;
 }
