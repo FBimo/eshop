e76feb1dc35f8d7ee59d414927c9f755540fe23e
FBimo <f.bimo31@gmail.com>
2024-03-06 20:57:57 +0700

    [REFACTOR] Fix import problem

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryTest.java
@@ -13,6 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest
 public class PaymentRepositoryTest {
